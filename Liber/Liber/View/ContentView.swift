192699cf9e4ebcd6d9e6c1513e6883dc98211c34
KEITA Modibo <Modykeita2@gmail.com>
2025-08-06 20:31:56 +0200

    mise à jour de l'image de fond

--- Liber/Liber/View/ContentView.swift
+++ Liber/Liber/View/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color("background")
+            Color("backgroundColor")
             VStack {
                 Label("Livre", systemImage: "book")
             }
