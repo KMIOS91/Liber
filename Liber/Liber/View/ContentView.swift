d5c217988ee1cb956d7621add8869f674d7f408b
KEITA Modibo <Modykeita2@gmail.com>
2025-08-06 19:30:31 +0200

    utilisation de la couleur

--- Liber/Liber/View/ContentView.swift
+++ Liber/Liber/View/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            Color("background")
             VStack {
                 Label("Livre", systemImage: "book")
             }
