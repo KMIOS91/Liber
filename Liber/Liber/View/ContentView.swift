16e7009ac9e14b2b5a00e7dabe8fbdcebd262d10
KEITA Modibo <Modykeita2@gmail.com>
2025-08-21 23:11:17 +0200

    nouvelle ignoresSafeArea

--- Liber/Liber/View/ContentView.swift
+++ Liber/Liber/View/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color("backgroundColor")
+                .ignoresSafeArea()
             VStack {
                 Label("Livre", systemImage: "book")
             }
